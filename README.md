1f144211b5593040ac099b805aab49ce57e52fc9
liang <yeslaw90@hotmail.com>
2021-12-04 10:24:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Project Details:
+Crypto app for news and price.
+
+Tools:
+
+1. React-App
+2. Using Rapid Api
+3. Using antdesign
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello This is your first crypto app.</h1>
+    </div>
+  );
+};
+
+export default App;
